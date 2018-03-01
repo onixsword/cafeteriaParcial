9e9ec90490d3ad2bbd45753ffc3e9a79e0f96304
Maestro <Maestro@iMac-de-Maestro-102.local>
2018-02-28 17:36:39 -0700

    Primer commit

---
+++ resources/views/comidas/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Comidas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <h1>Comidas</h1>
+    <button>Agregar comida</button>
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($comidas as $comida)
+                <tr>
+                    <td>{{$comida->nombre}}</td>
+                    <td>{{$comida->precio}}</td>
+                    <td><button>Editar</button></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
